2cf97b878143e08b9bf8c7cf08576d663a0e60c6
Gokul K <73621237+gokulkalai-web@users.noreply.github.com>
2024-02-03 13:33:13 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'  
-//import Todo from './To-do-Task/Todo'   // task 4
-import Cards from './Shopping-card-Task/Shopping-card'  //task 2 done
+import Todo from './To-do-Task/Todo'   // task 4
+//import Cards from './Shopping-card-Task/Shopping-card'  //task 2 done
 //import MainContent from './Component-Task/component'  //task 3 done
 //import  Mainroute from './routing.jsx'
 //import ParentComp from './context.jsx'
@@ -12,6 +12,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <Cards/>
+   <Todo/>
   </React.StrictMode>,
 )
